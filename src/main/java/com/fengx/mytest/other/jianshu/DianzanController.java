64f9921b19d4c7893d9b023d9f3024551edb317d
fengxin <930926134@qq.com>
2022-05-17 11:03:17 +0800

    jianshu:补充自动抽奖

--- src/main/java/com/fengx/mytest/other/jianshu/DianzanController.java
+++ src/main/java/com/fengx/mytest/other/jianshu/DianzanController.java
@@ -39,7 +39,7 @@ public class DianzanController {
      */
     private static final String vDianZan = "_3oieia";
 
-    private static String Cookie = "_ga=GA1.2.538624253.1644386005; UM_distinctid=17edd0b33c068d-057a92641ba9bb-33544874-1fa400-17edd0b33c1a50; __yadk_uid=tvbZYGY5BLMbSxzU61kuvARpFzCllWhz; web_login_version=MTY0ODA4NDUxMA%3D%3D--9592b6027812044ecce7a71d7373ae3a38f2f63c; _gid=GA1.2.441074158.1649216108; remember_user_token=W1sxOTYxOTM4OF0sIiQyYSQxMSRMODdtN1ZkZWp2TER0YjF1b0lYVC9PIiwiMTY0OTM4MzA2OS41MDg4Njk0Il0%3D--630ef01daa25b7df8465c05f00f84d002ebcfcdf; read_mode=day; default_font=font2; locale=zh-CN; _m7e_session_core=0543381d85a067212d8bdc7ec7b72daf; Hm_lvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1648789461,1649238087,1649322037,1649383072; CNZZDATA1279807957=1690344059-1644375653-https%253A%252F%252Fwww.baidu.com%252F%7C1649398055; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2219619388%22%2C%22first_id%22%3A%2217edd0b30c4efa-004957b4186591-33544874-2073600-17edd0b30c5358%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_utm_source%22%3A%22recommendation%22%2C%22%24latest_utm_medium%22%3A%22seo_notes%22%2C%22%24latest_utm_campaign%22%3A%22maleskine%22%2C%22%24latest_utm_content%22%3A%22note%22%7D%2C%22%24device_id%22%3A%2217edd0b30c4efa-004957b4186591-33544874-2073600-17edd0b30c5358%22%7D; Hm_lpvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1649404886";
+    private static String Cookie = "_ga=GA1.2.538624253.1644386005; UM_distinctid=17edd0b33c068d-057a92641ba9bb-33544874-1fa400-17edd0b33c1a50; __yadk_uid=tvbZYGY5BLMbSxzU61kuvARpFzCllWhz; _gid=GA1.2.1725469842.1652079791; CNZZDATA1279807957=1690344059-1644375653-https%253A%252F%252Fwww.baidu.com%252F%7C1652168821; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2219619388%22%2C%22first_id%22%3A%2217edd0b30c4efa-004957b4186591-33544874-2073600-17edd0b30c5358%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_utm_source%22%3A%22index-aside-click%22%2C%22%24latest_utm_medium%22%3A%22desktop%22%2C%22%24latest_utm_campaign%22%3A%22maleskine%22%2C%22%24latest_utm_content%22%3A%22note%22%2C%22%24latest_referrer_host%22%3A%22%22%7D%2C%22%24device_id%22%3A%2217edd0b30c4efa-004957b4186591-33544874-2073600-17edd0b30c5358%22%7D; Hm_lvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1652235849,1652237445,1652245340,1652317219; Hm_lpvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1652317219; remember_user_token=W1sxOTYxOTM4OF0sIiQyYSQxMSRMODdtN1ZkZWp2TER0YjF1b0lYVC9PIiwiMTY1MjMxNzIxNi45NDcyMjU4Il0%3D--33d6d49996971ca8a3cc763c997a940905e020b7; locale=zh-CN; _m7e_session_core=a586c8ca0441d8dac312ca54c3c3a9e2";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -54,6 +54,7 @@ public class DianzanController {
     @Scheduled(cron = "0 0 */6 * * ?")
     @GetMapping("/index")
     public void test() {
+        choujiang();
         exit = false;
         log.info("开始点赞..." + LocalDateTime.now());
         List<String> ids = Lists.newArrayList();
@@ -203,6 +204,37 @@ public class DianzanController {
         }
     }
 
+    /**
+     * 抽奖
+     */
+    private void choujiang() {
+        log.info("开始抽奖...");
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set("Accept", "application/json");
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            JSONObject object = new JSONObject();
+            object.put("fuc", 1);
+            String http = http(httpHeaders, "https://www.jianshu.com/asimov/user_ad_reward/v2_draw", HttpMethod.POST, object);
+            JSONObject object1 = JSONObject.parseObject(http);
+            if (object1.containsKey("error")) {
+                log.info("抽奖时间未到");
+            } else {
+                int left_count = object1.getIntValue("left_count");
+                if (left_count > 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    choujiang();
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private String fangwen(String url) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
@@ -218,7 +250,7 @@ public class DianzanController {
     }
 
     private <T> String http(HttpHeaders headers, String url, HttpMethod method, T body) {
-        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3883.400 QQBrowser/10.8.4582.400");
+        headers.set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Mobile Safari/537.36");
         headers.set("Cookie", Cookie);
         HttpEntity<T> entity;
         if (body == null) {
